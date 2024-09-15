5442e917aec0749c8514eaa2d9e6e4e07ddd8e38
KazuoYuuka <107776019+KazuoID@users.noreply.github.com>
2024-09-15 15:38:25 +0700

    add

---
+++ statis-app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace statis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f;
+            a = 20;
+            b = 20;
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+
+            double g, h;
+            g = Convert.ToDouble(10);
+            h = Math.Sqrt(g);
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(h);
+        }
+    }
+}
+
